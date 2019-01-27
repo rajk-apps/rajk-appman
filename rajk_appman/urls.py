c4eb771adfc1adad79cb92a9a827fe1170237b1f
Endre Mark Borza <endremborza@gmail.com>
2019-01-27 19:27:22 +0100

    v0.0.3

--- rajk_appman/urls.py
+++ rajk_appman/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path,include
 from . import views
 
+app_name = 'rajk-appman'
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('user_page', views.user_page, name='user_page')
 ]
--- rajk_appman/views.py
+++ rajk_appman/views.py
@@ -1,6 +1,30 @@
 from django.shortcuts import render
 from .models import App
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.forms import PasswordChangeForm
+from django.shortcuts import render, redirect
 
 def home(request):
     return render(request, 'rajk-appman/home.html',
                    {'applist': App.objects.all()})
+
+
+
+@login_required
+def user_page(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)  # Important!
+            messages.success(request, 'Your password was successfully updated!')
+            return redirect('rajk-appman:user_page')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'rajk-appman/user_page.html', {
+        'form': form
+    })
